4a1ca1f3dcbdb7797dd0b5c8f9b1372eea6327e5
dizonjudymarie-cyber <dizonjudymarie@gmail.com>
2026-04-24 13:21:13 +0800

    Add files via upload

---
+++ src/main/java/com/ws101/dizoncolele/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.ws101.dizoncolele.ecommerceapi.exception;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleAll(Exception ex) {
+
+        Map<String, Object> error = new HashMap<>();
+        error.put("timestamp", LocalDateTime.now());
+        error.put("message", ex.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+}
